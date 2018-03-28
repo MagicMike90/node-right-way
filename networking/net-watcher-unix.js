ead44accdb4bb2f802ec24cb4bb20d76ac715b3d
MagicMike90 <lcw303661398@hotmail.com>
2018-03-28 22:23:39 +1100

    add network protocol

---
+++ networking/net-watcher-unix.js
@@ -0,0 +1,23 @@
+'user strict';
+const fs = require('fs');
+const net = require('net');
+const filename = process.argv[2];
+
+if (!filename) {
+    throw Error('Error:No Filename specified.');
+}
+
+net.createServer(connection => {
+    // Reporting.
+    console.log('Subscriber conneted.');
+    connection.write(`Now watching "${filename}" for changes...\n`);
+
+    // Watcher setup.
+    const watcher = fs.watch(filename, () => connection.write(`File changed:${new Date()}\n`));
+
+    // Cleanup
+    connection.on('close', () => {
+        console.log('Subscriber disconnected.');
+        watcher.close();
+    });
+}).listen('/tmp/watcher.sock', () => console.log('Listening for subscribers...'));
